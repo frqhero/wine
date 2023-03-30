2ffef35548cc2995840bc3d3f46b63871b6bae0d
frqhero <frqhero@gmail.com>
2023-03-31 02:24:01 +0600

    Complete step #13

--- test.py
+++ test.py
@@ -1,4 +1,15 @@
 import pandas
+from pprint import pprint
 
-excel_data_df = pandas.read_excel('wine.xlsx', sheet_name='Лист1')
-x = 1
+excel_data_df = pandas.read_excel(
+    'wine2.xlsx', 
+    sheet_name='Лист1',
+    keep_default_na=False).to_dict(orient='records')
+res = {}
+for excel_line in excel_data_df:
+    category = excel_line['Категория']
+    curr_list = res.get(category, [])
+    curr_list.append(excel_line)
+    res[category] = curr_list
+
+pprint(res)
