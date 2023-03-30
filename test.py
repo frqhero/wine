57d0c91594044e3237e1d0007cd2009d25cf0a54
frqhero <frqhero@gmail.com>
2023-03-31 02:29:40 +0600

    Introduce defaultdict into the play

--- test.py
+++ test.py
@@ -1,15 +1,17 @@
-import pandas
 from pprint import pprint
+from collections import defaultdict
+
+import pandas
+
 
 excel_data_df = pandas.read_excel(
     'wine2.xlsx', 
     sheet_name='Лист1',
-    keep_default_na=False).to_dict(orient='records')
-res = {}
+    keep_default_na=False
+    ).to_dict(orient='records')
+res = defaultdict(list)
 for excel_line in excel_data_df:
     category = excel_line['Категория']
-    curr_list = res.get(category, [])
-    curr_list.append(excel_line)
-    res[category] = curr_list
+    res[category].append(excel_line)
 
 pprint(res)
